3aaeeaa51c2c246bb03c9c0277b634c7f081637c
Tushar-Mahata <kumar15-7252@diu.edu.bd>
2024-04-12 04:52:11 +0600

    attributes

--- oop_2.py
+++ oop_2.py
@@ -4,10 +4,18 @@ class Dog:
 
     def __init__(self, name):
         self.name = name
-        print(self.name)
+        #print(self.name)
+
+    def get_name(self):
+        return self.name
+
+    def set_name(self, name):
+        self.name = name
 
     def add_dog(self, x):
         return x + 1
+    
+
 
     def bark(self):
         print('ghew')
@@ -19,3 +27,11 @@ class Dog:
 #print(d.add_dog(7))
 d1 = Dog('Luffi')
 #print(d1)
+print(d1.name)
+print(d1.get_name())
+d1.set_name('Tim')
+print(d1.get_name())
+
+dogs_name = ['Mona', 'Tiger', 'Harry']
+dogs = Dog(dogs_name)
+print(dogs.get_name())
