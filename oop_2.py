c6fb24eb49572dc2a91fbc866420f7d30ef30d96
Tushar-Mahata <kumar15-7252@diu.edu.bd>
2024-04-12 04:26:00 +0600

    init

--- oop_2.py
+++ oop_2.py
@@ -2,6 +2,10 @@
 
 class Dog:
 
+    def __init__(self, name):
+        self.name = name
+        print(self.name)
+
     def add_dog(self, x):
         return x + 1
 
@@ -9,7 +13,9 @@ class Dog:
         print('ghew')
 
 
-d = Dog()
-d.bark()
-print(d.bark()) # this print returns with ghew and none
-print(d.add_dog(7))
+#d = Dog() #missing requirment Argumant
+#d.bark()
+#print(d.bark()) # this print returns with ghew and none
+#print(d.add_dog(7))
+d1 = Dog('Luffi')
+#print(d1)
