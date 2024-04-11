fd8c1cd47f5facf001af81fc77f39f59298780f1
Tushar-Mahata <kumar15-7252@diu.edu.bd>
2024-04-12 03:54:55 +0600

    Object and types

---
+++ oop_1.py
@@ -0,0 +1,10 @@
+print('tushar')
+x=7
+print(x)
+print(type(x))
+def med():
+    return 'method'
+print(f'printing the type of variable what return by medthod:', type(med()))
+print(type(med))
+print(med())
+print(x + med()) # typeError
